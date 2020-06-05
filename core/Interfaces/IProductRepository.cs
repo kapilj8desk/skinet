356de463f5d29fa95c0e91c3125e4629bfb674e8
kapil <kapiljoshi84@gmail.com>
2020-06-05 14:57:54 +0530

    End of creating seed section

---
+++ core/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using core.Entities;
+
+namespace core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+
+        Task<IReadOnlyList<ProductBrand>> GetProductsBrandAsync();
+        Task<IReadOnlyList<ProductType>> GetProductsTypeAsync();
+
+
+        
+    }
+}
